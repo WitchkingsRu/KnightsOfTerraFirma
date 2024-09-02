e21d9ea17c3b5ab6297cf62a44259f398ea67fbf
WitchkingsRu <vovakla647@gmail.com>
2024-09-03 00:46:58 +0300

    Armor mixins done

--- src/main/java/net/witchkings/knightsofterrafirma/item/EKArmor.java
+++ src/main/java/net/witchkings/knightsofterrafirma/item/EKArmor.java
@@ -7,9 +7,98 @@ public class EKArmor {
     public static void init() {
         // Call the method to apply all armor modifications at once
         ArmorModifier.modifyArmorType(ArmorTypes.KNIGHT,
-                300, 400, 450, 350,  // Durability values
-                3, 6, 9, 3, 1.25F, 2.5F);         // Defense values);
+                460, 630, 670, 550,  
+                2, 5, 7, 2, 1.25F, 2.5F);         // Defense values);
+        ArmorModifier.modifyArmorType(ArmorTypes.ARMET,
+                460, 630, 670, 550,  
+                2, 5, 7, 2, 1.25F, 2.5F);
 
-        // Other initialization code for your mod can go here
+        ArmorModifier.modifyArmorType(ArmorTypes.STECHHELM,
+                690, 880, 1000, 770,  
+                2, 6, 7, 3, 2F, 7.5F);
+        ArmorModifier.modifyArmorType(ArmorTypes.JOUSTING,
+                690, 880, 1000, 770,  
+                2, 6, 7, 3, 2F, 7.5F);
+
+        ArmorModifier.modifyArmorType(ArmorTypes.SALLET,
+                460, 630, 670, 550,  
+                2, 5, 7, 2, 1.25F, 2.5F);
+        ArmorModifier.modifyArmorType(ArmorTypes.GOTHIC,
+                460, 630, 670, 550,  
+                2, 5, 7, 2, 1.25F, 2.5F);
+
+        ArmorModifier.modifyArmorType(ArmorTypes.MAXIMILIAN_HELMET,
+                690, 880, 1000, 770,
+                2, 6, 7, 3, 1.8F, 2.5F);
+        ArmorModifier.modifyArmorType(ArmorTypes.MAXIMILIAN,
+                690, 880, 1000, 770,
+                2, 6, 7, 3, 1.8F, 2.5F);
+
+        ArmorModifier.modifyArmorType(ArmorTypes.CHAINMAIL,
+                340, 470, 500, 410,
+                1, 4, 5, 2, 0F, 0F);
+
+        ArmorModifier.modifyArmorType(ArmorTypes.KETTLEHAT,
+                400, 460, 580, 480,
+                2, 4, 6, 2, 0.5F, 0F);
+        ArmorModifier.modifyArmorType(ArmorTypes.PLATEMAIL,
+                400, 460, 500, 480,
+                2, 4, 6, 2, 0.5F, 0F);
+
+        ArmorModifier.modifyArmorType(ArmorTypes.BARBUTE,
+                360, 580, 630, 340,
+                2, 3, 6, 2, 0.5F, 0F);
+        ArmorModifier.modifyArmorType(ArmorTypes.HALFARMOR,
+                360, 580, 630, 340,
+                2, 3, 6, 2, 0.5F, 0F);
+
+        ArmorModifier.modifyArmorType(ArmorTypes.CRUSADER,
+                300, 560, 590, 440,
+                1, 4, 6, 3, 0.6F, 0F);
+
+        ArmorModifier.modifyArmorType(ArmorTypes.BRIGANDINE,
+                200, 400, 530, 340,
+                2, 3, 5, 2, 0.25F, 0F);
+
+        ArmorModifier.modifyArmorType(ArmorTypes.CEREMONIAL_ARMET,
+                560, 730, 770, 650,
+                2, 5, 7, 2, 1.25F, 2.5F);
+        ArmorModifier.modifyArmorType(ArmorTypes.CEREMONIAL,
+                560, 730, 770, 650,
+                2, 5, 7, 2, 1.25F, 2.5F);
+
+        ArmorModifier.modifyArmorType(ArmorTypes.SHISHAK,
+                400, 400, 540, 420,
+                2, 4, 5, 2, 0.3F, 0F);
+        ArmorModifier.modifyArmorType(ArmorTypes.NORMAN,
+                400, 400, 540, 400,
+                2, 4, 5, 2, 0.3F, 0F);
+
+        ArmorModifier.modifyArmorType(ArmorTypes.BASCINET,
+                420, 600, 640, 500,
+                2, 5, 6, 2, 1.25F, 0F);
+        ArmorModifier.modifyArmorType(ArmorTypes.XIV_CENTURY_KNIGHT,
+                420, 600, 640, 500,
+                2, 5, 6, 2, 1.25F, 0F);
+
+        ArmorModifier.modifyArmorType(ArmorTypes.WINGED_HUSSAR_CHESTPLATE,
+                420, 600, 640, 500,
+                2, 5, 6, 2, 1.25F, 0F);
+
+        ArmorModifier.modifyArmorType(ArmorTypes.CUIRASSIER,
+                300, 200, 630, 340,
+                1, 1, 6, 2, 0.5F, 0F);
+
+        ArmorModifier.modifyArmorType(ArmorTypes.KASTENBRUST,
+                460, 630, 670, 550,  
+                2, 5, 7, 2, 1.25F, 2.5F);
+        ArmorModifier.modifyArmorType(ArmorTypes.GRAND_BASCINET,
+                460, 630, 670, 550,  
+                2, 5, 7, 2, 1.25F, 2.5F);
+
+        ArmorModifier.modifyArmorType(ArmorTypes.LAMELLAR,
+                400, 440, 500, 480,
+                2, 5, 5, 2, 0.3F, 0F);
+
     }
 }
