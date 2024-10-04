6dd5278a9a44acc2a1ffa4d71afce2154877f65b
WitchkingsRu <vovakla647@gmail.com>
2024-10-04 18:28:39 +0300

    Armor almost done

--- src/main/java/net/witchkings/knightsofterrafirma/misc/Additions.java
+++ src/main/java/net/witchkings/knightsofterrafirma/misc/Additions.java
@@ -2,6 +2,7 @@ package net.witchkings.knightsofterrafirma.misc;
 
 
 
+import dev.architectury.platform.Platform;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -22,33 +23,39 @@ import java.util.function.Supplier;
 public class Additions {
     public static final DeferredRegister<CreativeModeTab> REGISTRY = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, KnightsOfTerraFirma.MODID);
 
+
     @SubscribeEvent
     public static void buildTabContentsVanilla(BuildCreativeModeTabContentsEvent tabData) {
-        if (tabData.getTabKey() == CreativeModeTabs.INGREDIENTS) {
-            tabData.getEntries().putAfter(ModdedItems.ROSE_GOLD_TEMPLATE.get().getDefaultInstance(), ModdedItems.PLATINUM_TEMPLATE.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+        if (Platform.isModLoaded("dfc")) {
+            if (tabData.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+                tabData.getEntries().putAfter(ModdedItems.ROSE_GOLD_TEMPLATE.get().getDefaultInstance(), ModdedItems.PLATINUM_TEMPLATE.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+            }
         }
     }
     @SubscribeEvent
     public static void buildTabContentsModded(BuildCreativeModeTabContentsEvent tabData) {
         if (tabData.getTabKey() == CreativeTab.KOTF_ARMOR.getKey()) {
-            tabData.getEntries().putAfter(ModelledItems.ROSE_GOLD_STEEL_CEREMONIAL_BOOTS.get().getDefaultInstance(), AdditionalItems.PLATINUM_STEEL_ARMET_WITH_PLUME_SUPPLIER.get(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
-            tabData.getEntries().putAfter(AdditionalItems.PLATINUM_STEEL_ARMET_WITH_PLUME_SUPPLIER.get(), AdditionalItems.PLATINUM_STEEL_CEREMONIAL_CHESTPLATE.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
-            tabData.getEntries().putAfter(AdditionalItems.PLATINUM_STEEL_CEREMONIAL_CHESTPLATE.get().getDefaultInstance(), AdditionalItems.PLATINUM_STEEL_CEREMONIAL_BOOTS.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+            if (Platform.isModLoaded("dfc")) {
+                tabData.getEntries().putAfter(ModelledItems.ROSE_GOLD_STEEL_CEREMONIAL_BOOTS.get().getDefaultInstance(), AdditionalItems.PLATINUM_STEEL_ARMET_WITH_PLUME_SUPPLIER.get(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+                tabData.getEntries().putAfter(AdditionalItems.PLATINUM_STEEL_ARMET_WITH_PLUME_SUPPLIER.get(), AdditionalItems.PLATINUM_STEEL_CEREMONIAL_CHESTPLATE.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+                tabData.getEntries().putAfter(AdditionalItems.PLATINUM_STEEL_CEREMONIAL_CHESTPLATE.get().getDefaultInstance(), AdditionalItems.PLATINUM_STEEL_CEREMONIAL_BOOTS.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
 
-            tabData.getEntries().putAfter(ModelledItems.ROSE_GOLD_BLACKSTEEL_KNIGHT_BOOTS.get().getDefaultInstance(), AdditionalItems.PLATINUM_BLACK_STEEL_ARMET_WITH_PLUME_SUPPLIER.get(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
-            tabData.getEntries().putAfter(AdditionalItems.PLATINUM_BLACK_STEEL_ARMET_WITH_PLUME_SUPPLIER.get(), AdditionalItems.PLATINUM_BLACKSTEEL_KNIGHT_CHESTPLATE.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
-            tabData.getEntries().putAfter(AdditionalItems.PLATINUM_BLACKSTEEL_KNIGHT_CHESTPLATE.get().getDefaultInstance(), AdditionalItems.PLATINUM_BLACKSTEEL_KNIGHT_LEGGINGS.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
-            tabData.getEntries().putAfter(AdditionalItems.PLATINUM_BLACKSTEEL_KNIGHT_LEGGINGS.get().getDefaultInstance(), AdditionalItems.PLATINUM_BLACKSTEEL_KNIGHT_BOOTS.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+                tabData.getEntries().putAfter(ModelledItems.ROSE_GOLD_BLACKSTEEL_KNIGHT_BOOTS.get().getDefaultInstance(), AdditionalItems.PLATINUM_BLACK_STEEL_ARMET_WITH_PLUME_SUPPLIER.get(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+                tabData.getEntries().putAfter(AdditionalItems.PLATINUM_BLACK_STEEL_ARMET_WITH_PLUME_SUPPLIER.get(), AdditionalItems.PLATINUM_BLACKSTEEL_KNIGHT_CHESTPLATE.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+                tabData.getEntries().putAfter(AdditionalItems.PLATINUM_BLACKSTEEL_KNIGHT_CHESTPLATE.get().getDefaultInstance(), AdditionalItems.PLATINUM_BLACKSTEEL_KNIGHT_LEGGINGS.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+                tabData.getEntries().putAfter(AdditionalItems.PLATINUM_BLACKSTEEL_KNIGHT_LEGGINGS.get().getDefaultInstance(), AdditionalItems.PLATINUM_BLACKSTEEL_KNIGHT_BOOTS.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
 
-            tabData.getEntries().putAfter(ModelledItems.BLUESTEEL_ROSE_GOLD_KNIGHT_BOOTS.get().getDefaultInstance(), AdditionalItems.PLATINUM_BLUE_STEEL_ARMET_WITH_PLUME_SUPPLIER.get(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
-            tabData.getEntries().putAfter(AdditionalItems.PLATINUM_BLUE_STEEL_ARMET_WITH_PLUME_SUPPLIER.get(), AdditionalItems.BLUESTEEL_PLATINUM_KNIGHT_CHESTPLATE.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
-            tabData.getEntries().putAfter(AdditionalItems.BLUESTEEL_PLATINUM_KNIGHT_CHESTPLATE.get().getDefaultInstance(), AdditionalItems.BLUESTEEL_PLATINUM_KNIGHT_LEGGINGS.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
-            tabData.getEntries().putAfter(AdditionalItems.BLUESTEEL_PLATINUM_KNIGHT_LEGGINGS.get().getDefaultInstance(), AdditionalItems.BLUESTEEL_PLATINUM_KNIGHT_BOOTS.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+                tabData.getEntries().putAfter(ModelledItems.BLUESTEEL_ROSE_GOLD_KNIGHT_BOOTS.get().getDefaultInstance(), AdditionalItems.PLATINUM_BLUE_STEEL_ARMET_WITH_PLUME_SUPPLIER.get(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+                tabData.getEntries().putAfter(AdditionalItems.PLATINUM_BLUE_STEEL_ARMET_WITH_PLUME_SUPPLIER.get(), AdditionalItems.BLUESTEEL_PLATINUM_KNIGHT_CHESTPLATE.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+                tabData.getEntries().putAfter(AdditionalItems.BLUESTEEL_PLATINUM_KNIGHT_CHESTPLATE.get().getDefaultInstance(), AdditionalItems.BLUESTEEL_PLATINUM_KNIGHT_LEGGINGS.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+                tabData.getEntries().putAfter(AdditionalItems.BLUESTEEL_PLATINUM_KNIGHT_LEGGINGS.get().getDefaultInstance(), AdditionalItems.BLUESTEEL_PLATINUM_KNIGHT_BOOTS.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
 
-            tabData.getEntries().putAfter(ModelledItems.REDSTEEL_ROSE_GOLD_KNIGHT_BOOTS.get().getDefaultInstance(), AdditionalItems.PLATINUM_RED_STEEL_ARMET_WITH_PLUME_SUPPLIER.get(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
-            tabData.getEntries().putAfter(AdditionalItems.PLATINUM_RED_STEEL_ARMET_WITH_PLUME_SUPPLIER.get(), AdditionalItems.REDSTEEL_PLATINUM_KNIGHT_CHESTPLATE.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
-            tabData.getEntries().putAfter(AdditionalItems.REDSTEEL_PLATINUM_KNIGHT_CHESTPLATE.get().getDefaultInstance(), AdditionalItems.REDSTEEL_PLATINUM_KNIGHT_LEGGINGS.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
-            tabData.getEntries().putAfter(AdditionalItems.REDSTEEL_PLATINUM_KNIGHT_LEGGINGS.get().getDefaultInstance(), AdditionalItems.REDSTEEL_PLATINUM_KNIGHT_BOOTS.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+                tabData.getEntries().putAfter(ModelledItems.REDSTEEL_ROSE_GOLD_KNIGHT_BOOTS.get().getDefaultInstance(), AdditionalItems.PLATINUM_RED_STEEL_ARMET_WITH_PLUME_SUPPLIER.get(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+                tabData.getEntries().putAfter(AdditionalItems.PLATINUM_RED_STEEL_ARMET_WITH_PLUME_SUPPLIER.get(), AdditionalItems.REDSTEEL_PLATINUM_KNIGHT_CHESTPLATE.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+                tabData.getEntries().putAfter(AdditionalItems.REDSTEEL_PLATINUM_KNIGHT_CHESTPLATE.get().getDefaultInstance(), AdditionalItems.REDSTEEL_PLATINUM_KNIGHT_LEGGINGS.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+                tabData.getEntries().putAfter(AdditionalItems.REDSTEEL_PLATINUM_KNIGHT_LEGGINGS.get().getDefaultInstance(), AdditionalItems.REDSTEEL_PLATINUM_KNIGHT_BOOTS.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+
+            }
 
         }
     }
